505ae33748d480c6711ceb65f4c10284ee15f477
Torben Jaeger <torben@jit-central.com>
2017-11-23 12:43:20 +0100

    formatting

--- src/test/java/com/redhat/brms/client/AppTest.java
+++ src/test/java/com/redhat/brms/client/AppTest.java
@@ -114,21 +114,21 @@ public class AppTest {
                 return Dish.class.isAssignableFrom(o.getClass());
             }
         }).forEach(entry -> {
-            Assert.assertEquals("result not expected", expected, ((Dish)entry).getName());
+            Assert.assertEquals("result not expected", expected, ((Dish) entry).getName());
         });
     }
 
     private void printResults(ServiceResponse<ExecutionResults> response) {
         //        Assert.assertEquals(2, response.getResult().getValue("fire-identifier"));
-//        response.getResult().getIdentifiers().stream().forEach(i -> {
-//            System.out.println(i);
-//            response.getResult().getFactHandle(i)
-//            response.getResult().getValue(i);
-//        });
+        //        response.getResult().getIdentifiers().stream().forEach(i -> {
+        //            System.out.println(i);
+        //            response.getResult().getFactHandle(i)
+        //            response.getResult().getValue(i);
+        //        });
 
-//        System.out.println(response.getResult().getValue("result"));
-//        System.out.println(response.getMsg());
-//        System.out.println(response.getType());
+        //        System.out.println(response.getResult().getValue("result"));
+        //        System.out.println(response.getMsg());
+        //        System.out.println(response.getType());
     }
 
     private ServiceResponse<ExecutionResults> getStringServiceResponse(String season, int numberOfGuests) {
@@ -142,8 +142,8 @@ public class AppTest {
                 .get().getCommands().newFireAllRules("fire-identifier"));
         commands.add((GenericCommand<?>) KieServices.Factory
                 .get().getCommands().newGetObjects("result"));
-//        commands.add((GenericCommand<?>) KieServices.Factory
-//                .get().getCommands().newGetObjects(new ClassObjectFilter(Dish.class),"outcome"));
+        //        commands.add((GenericCommand<?>) KieServices.Factory
+        //                .get().getCommands().newGetObjects(new ClassObjectFilter(Dish.class),"outcome"));
 
         BatchExecutionCommand batchCommand = KieServices.Factory
                 .get().getCommands().newBatchExecution(commands, session);
